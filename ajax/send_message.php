ef86fdf5cb32998c50dfe52814a789a52e9f3911
ubaidillah <ubaidillah101106@gmail.com>
2026-04-29 11:07:29 +0700

    Implement file upload in send_message.php
    Added file upload handling to send_message.php.

--- ajax/send_message.php
+++ ajax/send_message.php
@@ -3,12 +3,21 @@ include "../config/db.php";
 
 $room = $_POST['room'];
 $sender = $_POST['sender'];
-$nama = $_POST['nama'];
+$nama = $_POST['nama'] ?? '';
 $message = $_POST['message'] ?? '';
 $lat = $_POST['lat'] ?? null;
 $lng = $_POST['lng'] ?? null;
 
+$fileName = "";
+
+// 🔥 HANDLE FILE
+if (!empty($_FILES['file']['name'])) {
+    $fileName = time() . "_" . $_FILES['file']['name'];
+    move_uploaded_file($_FILES['file']['tmp_name'], "../uploads/chat/" . $fileName);
+}
+
+// 🔥 INSERT
 mysqli_query($conn, "INSERT INTO chat 
-(room_id, sender, nama_pengirim, message, latitude, longitude) 
+(room_id, sender, nama_pengirim, message, latitude, longitude, file, status)
 VALUES 
-('$room','$sender','$nama','$message','$lat','$lng')");
+('$room','$sender','$nama','$message','$lat','$lng','$fileName','sent')");
